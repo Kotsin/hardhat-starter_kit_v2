55881b35075b0b5a34555e28ce3ddd4390f101ce
Denis Kotsin <kdo5@mail.ru>
2023-08-18 16:18:21 +0700

    config modification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,18 +13,42 @@ import { HardhatUserConfig } from 'hardhat/types'
 
 dotenv.config()
 
+const forkingConfig = {
+  url: <string>process.env.FORK_URL,
+  // blockNumber: parseInt(process.env.FORK_BLOCK_NUMBER!),
+}
+
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
+  namedAccounts: {
+    deployer: 0,
+    owner: 1,
+    user: 2,
+  },
   networks: {
+    hardhat: {
+      chainId: process.env.FORK ? parseInt(process.env.FORK_CHAINID!) : 31337,
+      // gas: "auto",
+      // allowUnlimitedContractSize: true,
+      forking: process.env.FORK ? forkingConfig : undefined,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        accountsBalance: '1000000000000000000000000000000',
+      },
+      live: false,
+      saveDeployments: false,
+      tags: ['hardhat'],
+    },
     sepolia: {
-      url: process.env.JSON_RPC_PROVIDER_URL ?? '',
+      url: process.env.JSON_RPC_PROVIDER_URL ?? 'https://rpc2.sepolia.org	',
+      gasPrice: 'auto',
       verify: {
         etherscan: {
           apiKey: process.env.ETHERSCAN_API_KEY,
         },
       },
       accounts: {
-        mnemonic: process.env.MNEMONIC ?? '',
+        mnemonic: process.env.MNEMONIC ?? 'test test test test test test test test test test test junk',
         count: 20,
       },
     },
